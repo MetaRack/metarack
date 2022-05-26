34ddaec8fdd0bc72be721665775f613f85467da9
ferluht <ilya.belikov@phystech.edu>
2022-05-27 01:50:47 +0400

    ssl

---
+++ https_redirect.py
@@ -0,0 +1,14 @@
+import uvicorn
+from fastapi import FastAPI
+from starlette.requests import Request
+from starlette.responses import RedirectResponse
+
+app = FastAPI()
+
+
+@app.route('/{_:path}')
+async def https_redirect(request: Request):
+    return RedirectResponse(request.url.replace(scheme='https'))
+
+if __name__ == '__main__':
+    uvicorn.run('https_redirect:app', host='0.0.0.0', port=8080, reload=True)
