435e0b970bf5468f60b0f58cd9b98670d74ca9f1
ferluht <ilya.belikov@phystech.edu>
2022-05-27 12:35:46 +0400

    git puller

--- server/src/git_puller.py
+++ server/src/git_puller.py
@@ -1,13 +1,35 @@
+import os
 import time
-from build import checkout, minify
+import shutil
+import subprocess
+
+def clone(user='ferluht', url='github.com/ferluht/', repo='metarack', token=''):
+    repo_url = os.path.join(url, repo)
+    if os.path.exists(repo) and os.path.isdir(repo):
+        shutil.rmtree(repo)
+    process = subprocess.Popen(["git", "clone", f"https://{user}:{token}@{repo_url}"], stdout=subprocess.PIPE)
+    output = process.communicate()[0]
+
+def checkout():
+    process = subprocess.Popen(["git", "pull", "--rebase"], stdout=subprocess.PIPE)
+    output = process.communicate()[0]
+    if (output.decode() == 'Already up to date.\n'):
+        return False
+    return True
 
 if __name__ == "__main__":
-    with open('private/github-token', 'r') as file:
+    with open('/github-token', 'r') as file:
         token = file.read().rstrip()
+
+    clone(user='ferluht', url='github.com/ferluht', repo='metarack', token=token)
+    os.chdir('metarack')
+
+    from metarack.build import minify
+
     try:
         while True:
-            if checkout(token):
-                minify()
+            if checkout():
+                minify(out_root='server/website/demo')
         time.sleep(int(os.getenv('GIT_PULL_INTERVAL')))
     except KeyboardInterrupt:
         print('exit')
