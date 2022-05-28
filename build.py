f6b4c02e5a333fb0908620d50fc1b867b8bfe97a
ferluht <ilya.belikov@phystech.edu>
2022-05-28 13:45:28 +0400

    wasm path

--- build.py
+++ build.py
@@ -32,8 +32,8 @@ def minify(prefix='./', out_root='./'):
     wasm_files = get_all_files(root=os.path.join(prefix, 'bin'), frmt='.wasm')
 
     wasm_bin_path = os.path.join(out_root, 'bin')
-    if not os.path.exists(wasm_bin_path):
-        os.makedirs(wasm_bin_path)
+    if os.path.exists(wasm_bin_path):
+        os.rmdir(wasm_bin_path)
     for f in wasm_files:
         shutil.copy(f, wasm_bin_path)
     merge_files(files=js_files, out='./metarack.js')
