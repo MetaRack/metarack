96b0a24ce951932af1b8a9f3aafdaf12aafb6c9a
ferluht <ilya.belikov@phystech.edu>
2022-05-30 21:29:46 +0400

    remove build

--- c-modules/build/3rdparty/libsamplerate/src/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/ferluht/Dev/metarack/metarack/c-modules/3rdparty/libsamplerate/src
-# Build directory: /Users/ferluht/Dev/metarack/metarack/c-modules/build/3rdparty/libsamplerate/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- c-modules/build/src/Plateau/CMakeFiles/Plateau.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for Plateau.
-# This may be replaced when dependencies are built.
